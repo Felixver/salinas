bcae92678a440da0c8849b9ef1ea082608ab2bb1
Felix Verhulst <felix.verhulst@hotmail.fr>
2018-06-01 17:35:04 +0200

    video

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'information', to: 'pages#information', as: :information
+
+  get 'pages/home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact', to: 'pages#contact', as: :contact
 end
